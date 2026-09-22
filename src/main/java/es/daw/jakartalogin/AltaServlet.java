09e689a7eefc7217728a87776a4b6243f358a40b
profeMelola <melola@educa.madrid.org>
2026-09-22 21:18:35 +0200

    Creado leerFichero

--- src/main/java/es/daw/jakartalogin/AltaServlet.java
+++ src/main/java/es/daw/jakartalogin/AltaServlet.java
@@ -1,6 +1,9 @@
 package es.daw.jakartalogin;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 
 import jakarta.servlet.ServletException;
@@ -17,6 +20,8 @@ public class AltaServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
+        // Leer el fichero de texto tecnologias.txt y cargar en un ArrayList
+        List<String> tecnologias = leerFichero();
 
 
     }
@@ -28,5 +33,40 @@ public class AltaServlet extends HttpServlet {
 
 
     }
+
+    private List<String> leerFichero(String rutaFichero) throws IOException{
+        List<String> lista = new ArrayList<>();
+
+        //InputStream is = getServletContext().getResourceAsStream("/WEB-INF/datos/tecnologia.txt");
+        InputStream is = getServletContext().getResourceAsStream(rutaFichero);
+
+        if (is == null)
+            throw new IOException("No se encuentra el fichero "+rutaFichero);
+
+        // try con recursos...
+        // BufferedReader br = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8))){
+           String linea;
+           while( (linea = br.readLine()) != null){
+               if (!linea.isBlank())
+                   lista.add(linea.trim());
+           }
+        }
+//        catch (IOException e){
+//            LOGGER.info(e.getMessage());
+//        }
+//        finally{
+//            // se cerraban recursos.... viejuno!!!!!
+//            try {
+//                br.close();
+//            } catch (IOException e) {
+//                LOGGER.info(e.getMessage());
+//            }
+//        }
+
+        return lista;
+    }
+
+
 
 }
